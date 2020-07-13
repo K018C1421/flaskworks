de0f6013f7dd05474076e4825d7924bf8ec41ef3
K018C1421 <k018c1421@it-neec.jp>
2020-07-13 14:20:07 +0900

    4.4.1.routingpass

--- app.py
+++ app.py
@@ -2,13 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/index/')
-def index():
-    return render_template('index.html', message="indexページ")
-
-@app.route('/hello/')
-def hello():
-    return render_template('index.html', message="Helloページ")
+@app.route('/user/<username>/')
+def profile(username):
+    return render_template('index.html', message=username)
 
 if __name__ == '__main__':
     app.debug = True
