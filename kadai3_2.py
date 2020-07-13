d55cefdfa27fec498416892ea46f3d6ded2d6191
K018C1421 <k018c1421@it-neec.jp>
2020-07-13 13:19:00 +0900

    kadai3_1

--- kadai3_2.py
+++ kadai3_2.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', num = 10)
+    list = {"‰pŒê":87, "”Šw":90, "‘Œê":45, "—‰È":76, "Ğ‰ï":31}
+    return render_template('index.html', list=list)
 
 if __name__ == '__main__':
     app.debug = True
