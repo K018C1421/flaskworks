080f07a11c3213e81698c59ca61e02e270ebb1bc
K018C1421 <k018c1421@it-neec.jp>
2020-07-13 14:10:33 +0900

    4.2.routing

--- kadai3_2/kadai3_2.py
+++ kadai3_2/kadai3_2.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
-@app.route('/index/')
+@app.route('/')
 def index():
-    list = {'apple':'りんご', 'orange':'みかん', 'lemon':'レモン'}
+    list = {"英語":87, "数学":90, "国語":45, "理科":76, "社会":31}
     return render_template('index.html', list=list)
 
 if __name__ == '__main__':
