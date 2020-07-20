b6c17cb4952730a2e583a93ace0560a250d26f57
K018C1421 <k018c1421@it-neec.jp>
2020-07-20 15:45:23 +0900

    kadai5_2

--- kadai5_2.py
+++ kadai5_2.py
@@ -7,7 +7,7 @@ numlist = []
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', length=0)
 
 @app.route('/', methods=['POST'])
 def send():
@@ -15,8 +15,7 @@ def send():
     dt_now = datetime.datetime.now()
     timestamp.append(dt_now.strftime('%m/%d %H:%M'))
     numlist.append(num)
-    return render_template('index.html', t=timestamp 
-    , n=numlist, len=len(numlist))
+    return render_template('index.html', t=timestamp, n=numlist, length=len(numlist))
 
 if __name__ == '__main__':
     app.debug = True
