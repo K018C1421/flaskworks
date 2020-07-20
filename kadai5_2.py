dbac74d07cbb53766e912e16b4f9561ffd5d9834
K018C1421 <k018c1421@it-neec.jp>
2020-07-20 13:59:15 +0900

    kadai5_2

---
+++ kadai5_2.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import datetime
+
+app = Flask(__name__)
+timestamp = []
+numlist = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def send():
+    num = request.form.get('num')
+    dt_now = datetime.datetime.now()
+    timestamp.append(dt_now.strftime('%m/%d %H:%M'))
+    numlist.append(num)
+    return render_template('index.html', t=timestamp 
+    , n=numlist, len=len(numlist))
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
